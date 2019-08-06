c737077711620398bda58434a4bea6f807a4527a
OlaSGustafsson <ola@eldstorpsolutions.se>
2019-08-06 22:14:06 +0200

    First commit

---
+++ src/build/precache-manifest.0d943fb37b72f36353234327f37d50f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15e630c81615e7b4d4f5b6b5677b88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d210698ccdd0a617ebc9",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "bb660b66d97ca872da68",
+    "url": "/static/js/2.ad55af6c.chunk.js"
+  },
+  {
+    "revision": "d210698ccdd0a617ebc9",
+    "url": "/static/js/main.901205d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ startMongoDB.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="C:\Users\ola\Code\React__Creating_And_Hosting_a_Full-Stack_Site\my-blog-backend\data\db"
+pause
+
+:: to start mongo shell, run this:
+:: "C:\Program Files\MongoDB\Server\4.0\bin\mongo.exe"
+:: quit with ^C
